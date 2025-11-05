a327deecc56f6e91a893b8ecfc1dd8f7184a9620
Odette Gallo <odette.gallo@hotmail.com>
2025-11-05 02:36:10 -0300

    se agrega url de cripto-vue

--- src/stores/usePortfolioStore.js
+++ src/stores/usePortfolioStore.js
@@ -50,8 +50,8 @@ export const usePortfolioStore = defineStore('portfolio', {
         name: 'Aplicación Web con Firebase (Proyecto Destacado)',
         description: 'Este proyecto ejemplifica mi capacidad para realizar un deploy completo de una aplicación web, integrando servicios back-end (autenticación y hosting).',
         technologies: ['Vue', 'Vue-Router', 'Firebase Hosting/Auth'],
-        demoUrl: 'https://cursos-adweb-online.web.app/login',
-        repoUrl: 'https://github.com/odettegallo/adweb-online-cursos.git',
+        demoUrl: 'https://cripto-vue.web.app/login',
+        repoUrl: 'https://github.com/odettegallo/cripto-vue.git',
       },
       {
         id: 2,
